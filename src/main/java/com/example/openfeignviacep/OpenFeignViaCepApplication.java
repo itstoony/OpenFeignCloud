c9dd88213fefe68883a485bfa81db96a4854675d
tony <toonyrenner@gmail.com>
2023-03-27 09:00:07 -0300

    creating project

---
+++ src/main/java/com/example/openfeignviacep/OpenFeignViaCepApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfeignviacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignViaCepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignViaCepApplication.class, args);
+    }
+
+}
